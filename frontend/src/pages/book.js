e9f7774018d22c4594bca34f3dfc0d807248b586
draptis <dimitrisraptis96@gmail.com>
2019-01-10 23:00:04 +0200

    room booking requests and alert messages

--- frontend/src/pages/book.js
+++ frontend/src/pages/book.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import axios from 'axios';
 import styled from 'styled-components';
+import swal from 'sweetalert';
 
 import { Link } from 'gatsby'
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
+import { Button } from "grommet";
 
 import Calendar from "../components/calendar";
 import Input from '../components/input'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import Card from '../components/card'
-
 
 const Underline = styled.span`
   background-image: linear-gradient(120deg, #55efc4, #55efc4 100%);
@@ -42,9 +44,11 @@ const CalendarWrapper = styled.div`
 
 class Book extends React.Component {
   state = {
-    unit: '',
-    building: '',
-    room: '',
+    unitId: '',
+    buildingId: '',
+    roomId: '',
+
+    isAvailable: false,
 
     price: '',
     numOfPeople: '',
@@ -72,6 +76,65 @@ class Book extends React.Component {
     this.setState({ dateTo: nextDate !== dateTo ? nextDate : undefined });
   };
 
+  checkAvailability = () => {
+    const {
+      roomId,
+      buildingId,
+      unitId,
+      dateFrom,
+      dateTo,
+    } = this.state;
+
+    axios.post('/checkRoom', {
+      roomId,
+      buildingId,
+      unitId,
+      dateTo,
+      dateFrom,
+    })
+      .then((response) => {
+        this.setState({isAvailable: true})
+        swal("Perfect!", "The room is available!  ", "success");
+      })
+      .catch((error) => {
+        this.setState({isAvailable: false})
+        swal("Oops!", "Sorry bro! These dates are reserved", "error");
+      });
+  }
+
+  submit = () => {
+    const {
+      roomId,
+      buildingId,
+      unitId,
+      dateTo,
+      dateFrom,
+      numOfPeople,
+      firstname,
+      lastname,
+      email,
+    } = this.state;
+
+    axios.post('http://fakeurl.com/bookd', {
+      roomId,
+      buildingId,
+      unitId,
+      dateTo,
+      dateFrom,
+      numOfPeople,
+      firstname,
+      lastname,
+      email,
+    })
+      .then((response) => {
+        this.setState({isAvailable: true})
+        swal("Have Fun!", "Your reservation was successful!","success");
+      })
+      .catch((error) => {
+        this.setState({isAvailable: false})
+        swal("Oops!", "Server error, bro!", "error");
+      });
+  }
   render(){
     // console.log(this.state);
 
@@ -81,6 +144,7 @@ class Book extends React.Component {
       email,
       dateFrom,
       dateTo,
+      isAvailable,
     } = this.state;
 
     console.log(this.state);
@@ -88,38 +152,51 @@ class Book extends React.Component {
     return (
       <Layout>
         <h2><Underline>Book a room</Underline></h2>
-        <p>Choose a room and fill your personal information:</p>
 
-        <Input 
-          value={firstname}
-          label={'Firstname'}
-          onChange={this.handleFirstname}
-        />
-        <Input 
-          value={lastname}
-          label={'Lastname'}
-          onChange={this.handleLastname}
-        />
-        <Input 
-          value={email}
-          label={'Email'}
-          onChange={this.handleEmail}
-        />  
+        { !isAvailable && 
+          <Fragment>
+            <p style={{margin: '2em 0'}}>Choose dates for your room:</p>
+            <CalendarWrapper>
+              <Calendar
+                label={'Check In'}
+                date={dateFrom}
+                endDate={dateTo}
+                onSelect={this.handleDateFrom}
+              />
+              <Calendar
+                label={'Check Out'}
+                date={dateTo}
+                startDate={dateFrom}
+                onSelect={this.handleDateTo}
+              />
+            </CalendarWrapper>
 
-        <CalendarWrapper>
-          <Calendar
-            label={'Check In'}
-            date={dateFrom}
-            endDate={dateTo}
-            onSelect={this.handleDateFrom}
-          />
-          <Calendar
-            label={'Check Out'}
-            date={dateTo}
-            startDate={dateFrom}
-            onSelect={this.handleDateTo}
-          />
-        </CalendarWrapper>
+            <Button style={{margin: '2em 0'}} color='#55efc4' label={'Check'} onClick={this.checkAvailability} />
+          </Fragment>
+        }
+
+        { isAvailable &&
+          <Fragment>
+            <p style={{margin: '2em 0'}}> Now fill your personal information:</p>
+            <Input 
+              value={firstname}
+              label={'Firstname'}
+              onChange={this.handleFirstname}
+            />
+            <Input 
+              value={lastname}
+              label={'Lastname'}
+              onChange={this.handleLastname}
+            />
+            <Input 
+              value={email}
+              label={'Email'}
+              onChange={this.handleEmail}
+            /> 
+
+            <Button style={{margin: '2em 0'}} color='#55efc4' label={'Book'} onClick={this.submit} />
+          </Fragment>
+        }
 
       </Layout>
     );
